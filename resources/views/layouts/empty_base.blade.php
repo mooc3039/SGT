f29075d5e93e45e4e1610fd176ecb8adf4d27a28
osoriocassiano <osoriocassiano@gmail.com>
2018-01-25 02:15:03 -0800

    Modelando o report a imagem da factura

--- resources/views/layouts/empty_base.blade.php
+++ resources/views/layouts/empty_base.blade.php
@@ -10,9 +10,10 @@
   <meta name="keyword" content="Gestão, Stock, Faturação, Produtos, Encomendas">
   <link rel="shortcut icon" href="img/favicon.png">
 
-  <title>SG-Stock</title>
+  <title>SG-Stock Administração</title>
   {!!Html::style('css/bootstrap.min.css')!!}
   {!!Html::style('css/bootstrap-theme.css')!!}
+  {!!Html::style('css/sgt.css')!!}
   {!!Html::style('css/elegant-icons-style.css')!!}
   {!!Html::style('css/font-awesome.min.css')!!}
   {!!Html::style('assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css')!!}
@@ -30,6 +31,9 @@
 </head>
   <body>
 
+    <section class="">
+          @yield('content')
+      </section>
 
     {!!Html::script('js/jquery.js')!!}
     {!!Html::script('js/jquery-ui-1.10.4.min.js')!!}
@@ -118,5 +122,137 @@
         });
       </script>
   </body>
+  <script type="text/javascript">
 
+    //====trocar de focus para o proximo campo a preencher
+    $('tbody').delegate('.descricao','change', function(){
+      var tr = $(this).parent().parent();
+      tr.find('.quantidade').focus();
+    });
+
+    //======pegar os valores dos campos e calcular o valor de cada produto====
+      $('tbody').delegate('.quantidade,.preco_venda,.desconto','keyup',function(){
+        var tr = $(this).parent().parent();
+        var quantidade = tr.find('.quantidade').val();
+        var preco_venda = tr.find('.preco_venda').val();
+        var desconto = tr.find('.desconto').val();
+        var subtotal = (quantidade*preco_venda)-(quantidade*preco_venda*desconto)/100;
+        tr.find('.subtotal').val(subtotal);
+        total();
+      });
+
+      //==calculo do total de todas as linhas
+        function total()
+        {
+          var total =0;
+          $('.subtotal').each(function(i,e){
+            var subtotal = $(this).val()-0;
+            total +=subtotal;
+          })
+          $('.total').html(total.formatMoney(2,',','.')+ " Mtn");
+        };
+
+        // ==== formatando os numeros ====
+        Number.prototype.formatMoney = function(decPlaces, thouSeparator, decSeparator){
+          var n = this,
+              decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+              decSeparator = decSeparator == undefined ? ".": decSeparator,
+              thouSeparator = thouSeparator == undefined ? ",": thouSeparator,
+              sign = n < 0 ? "-" : "",
+              i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+              j = (j = i.length) > 3 ? j % 3 : 0;
+              return sign + (j ? i.substr(0,j) + thouSeparator : "")
+              + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator)
+              + (decPlaces ? decSeparator + Math.abs(n-i).toFixed(decPlaces).slice(2) : "");
+        };
+//---começam aqui as funçoes que filtram somente números
+          //---find element by row--
+          function findRowNum(input){
+            $('tbody').delegate(input, 'keydown',function(){
+              var tr =$(this).parent().parent();
+              number(tr.find(input));
+            });
+          }
+
+          function findRowNumOnly(input){
+            $('tbody').delegate(input, 'keydown',function(){
+              var tr =$(this).parent().parent();
+              numberOnly(tr.find(input));
+            });
+          }
+
+      //--numeros e pontos
+      function number(input){
+        $(input).keypress(function (evt){
+          var theEvent = evt || window.event;
+          var key = theEvent.keyCode || theEvent.which;
+          key = String.fromCharCode( key );
+          var regex = /[-\d\.]/;
+          var objRegex = /^-?\d*[\.]?\d*$/;
+          var val = $(evt.target).val();
+          if(!regex.test(key) || !objRegex.test(val+key) ||
+            !theEvent.keyCode == 46 || !theEvent.keyCode == 8){
+              theEvent.returnValue = false;
+              if(theEvent.preventDefault) theEvent.preventDefault();
+            };
+        });
+      };
+        function findRowNumOnly(input){
+          $('tbody').delegate(input, 'keydown',function(){
+            var tr =$(this).parent().parent();
+            numberOnly(tr.find(input));
+          });
+        }
+        //-------------somente numeros
+            function numberOnly(input){
+              $(input).keypress(function(evt){
+                var e = event || evt;
+                var charCode = e.which || e.keyCode;
+                if (charCode > 31 && (charCode < 48 || charCode > 57))
+                return false;
+                return true;
+              });
+            }
+         //---limitando somente para entrada de números
+         findRowNum('.quantidade');
+         findRowNum('.preco_venda');
+         findRowNum('.desconto');   
+            
+
+  //------devolver dados do price
+      $('tbody').delegate('.descricao','change',function(){
+        var tr= $(this).parent().parent();
+        var id = tr.find('.descricao').val();
+        var dataId={'id':id};
+        $.ajax({
+          type  : 'GET',
+          url   : '{!!URL::route('findPrice')!!}',
+          dataType: 'json',
+          data  : dataId,
+          success:function(data){
+            tr.find('.preco_venda').val(data.preco_venda);
+          }
+        });
+      });
+
+    //==========adiciona mais uma linha da usando a função addRow==
+    $('.addRow').on('click',function(){
+      addRow();
+    });
+
+    //====remove a linha adicionada, foram corrigidos muitos bugs aqui===
+       
+    $('tbody').on('click','.remove',function(){
+          var l=$('tbody tr').length;
+          if (l==1) {
+            alert('Não poderá remover o ultimo campo de facturação');
+          }else{
+          $(this).parent().parent().remove(); 
+          total();
+          }
+          
+        });
+
+
+  </script>
 </html>
--- resources/views/saidas/show_saida.blade.php
+++ resources/views/saidas/show_saida.blade.php
@@ -1,101 +1,189 @@
 @extends('layouts.empty_base')
-
+@section('content')
 
 <div class="container">
-
-	<div class="row" style="background-color: white">
-		<div class="col-md-5 border">
-			P&SA<br>
-			Papelaria e Serviços Agenda
+	<div class="row">
+		<div class="col-md-12">
 			
-		</div>
-
-		<div class="col-md-4">
-			Nome do Cliente: {{$saida->cliente->nome}}<br>
-			Endereço: {{$saida->cliente->endereco}}<br>
-			Nuit: {{$saida->cliente->nuit}}<br>
-		</div>
-
-		<div class="col-md-3">
 			<div class="panel panel-default">
-				<div class="panel-heading">Numero da Saida:</div>
-				<div class="panel-body">
-					{{$saida->id}}
+				<div class="panel-heading">
+					<div class="row">
+						<div class="col-md-5 border">
+							<div class="panel panel-default">
+								
+								<div class="panel-body">
+									Papelaria e Serviços Agenda<hr>
+									Emal: papelaria@gmail.com<br>
+									Telefone: +218293503 / +258840294826<br>
+									Endereco: Av. 24 de Julho<br>
+								</div>
+							</div>
+
+							
+
+						</div>
+
+						<div class="col-md-4">
+							
+							<div class="panel panel-default">
+								<div class="panel-body text-center">
+									<h2> <b> Dados do Cliente </b></h2> <hr>
+									Nome do Cliente: {{$saida->cliente->nome}}<br>
+									Endereço: {{$saida->cliente->endereco}}<br>
+									Nuit: {{$saida->cliente->nuit}}<br>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-3">
+
+							<div class="panel panel-default">
+								<div class="panel-body text-center">
+									<h2> <b> Numero de Saida / Factura </b> </h2> <hr>
+									<h1>{{$saida->id}}</h1>
+								</div>
+							</div>
+
+
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6"> MAPUTO</div>
+						<div class="col-md-6 text-right"> Data: {{$saida->data}} </div>
+					</div>
 				</div>
+
+
+				<div class="panel-body">
+
+					<div class="row">
+						<div class="col-md-12">
+							<table class="table table-striped table-advance table-hover">
+								<tbody>
+
+									<tr>
+										<th><i class="icon_profile"></i>Quantidade</th>
+										<th><i class="icon_mobile"></i> Designação </th>
+										<th><i class="icon_mail_alt"></i> Preço Unitário </th>
+										<th><i class="icon_cogs"></i> Valor Total </th>
+									</tr>
+
+									@foreach($saida->itensSaida as $iten_saida)
+									<tr>
+										<td> {{$iten_saida->quantidade}} </td>
+										<td> {{$iten_saida->produto->descricao}} </td>
+										<td> {{$iten_saida->produto->preco_venda}} </td>
+										<td> {{$iten_saida->valor}} </td>
+
+									</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				<div class="panel-footer">
+					<div class="row">
+
+						<div class="col-md-6 border">
+
+
+							<div class="panel panel-default">
+								<div class="panel-body">
+									Motivo Justificativo da não aplicação de imposto:
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-md-6 text-right">
+
+							<table class="pull-right">
+								<tr>
+									<td>
+										Sub-Total:
+									</td>
+									<td>
+										{{$saida->subtotal}}
+									</td>
+								</tr>
+
+
+								<tr>
+									<td>
+										IVA:
+									</td>
+									<td>
+										17%
+									</td>
+								</tr>
+
+								<tr>
+									<td>
+										Desconto:
+									</td>
+									<td>
+										{{$saida->desconto}}
+									</td>
+								</tr>
+
+								<tr>
+									<td>
+										Valor Total:
+									</td>
+									<td>
+										<b>{{$saida->valor_total}}</b>
+									</td>
+								</tr>
+							</table>
+
+						</div>
+
+					</div>
+					<br><br>
+					<div class="row">
+
+						<div class="col-md-6">
+
+							<div class="panel panel-info">
+								<div class="panel-heading">
+									Datos bancarios
+								</div>
+								<div class="panel-body">
+									Conta BCI (MZN) 54169166 10 1<br>
+									Conta BIM (MZN) 5299/07<br>
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-md-6">
+
+							
+
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col-md-6"><a href="{{route('saida_pdf', $saida->id)}}" class="btn btn-primary">Imprimir Saída</a>
+
+					</div>
+					<div class="col-md-6 text-right"><a href="{{route('saida.index')}}" class="btn btn-warning">Cancelar</a>
+
+					</div>
+					</div>
+				</div>
+
+
 			</div>
-			 
+
+
 
 		</div>
 	</div>
-
-	<div class="row">
-		<div class="col-md-5"> MAPUTO</div>
-		<div class="col-md-4"> Data: {{$saida->data}} </div>
-		<div class="col-md-3"></div>
-	</div>
-
-
-	<div class="row">
-		<div class="col-md-12">
-			<table class="table table-striped table-advance table-hover">
-				<tbody>
-
-					<tr>
-						<th><i class="icon_profile"></i>Quantidade</th>
-						<th><i class="icon_mobile"></i> Designação </th>
-						<th><i class="icon_mail_alt"></i> Preço Unitário </th>
-						<th><i class="icon_cogs"></i> Valor Total </th>
-					</tr>
-
-					@foreach($saida->itensSaida as $iten_saida)
-					<tr>
-						<td> {{$iten_saida->quantidade}} </td>
-						<td> {{$iten_saida->produto->descricao}} </td>
-						<td> {{$iten_saida->produto->preco_venda}} </td>
-						<td> {{$iten_saida->valor}} </td>
-
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
-	</div>
-
-	<div class="row">
-
-		<div class="col-md-6">
-			
-			Motivo Justificativo da não aplicação de imposto.
-
-		</div>
-
-		<div class="col-md-6 text-right">
-
-			Sub-Total: {{$saida->subtotal}}<br>
-			Desconto: {{$saida->desconto}}<br>
-			Valor Total: {{$saida->valor_total}}<br>
-
-		</div>
-
-	</div>
-
-	<div class="row">
-
-		<div class="col-md-12">
-
-			Conta BCI (MZN) 54169166 10 1<br>
-			Conta BIM (MZN) 5299/07<br>
-
-		</div>
-
-	</div>
-
-	<div class="panel-footer"><a href="{{route('saida_pdf', $saida->id)}}" class="btn btn-primary">Imprimir Saída</a>
-
-	</div>
-
 </div>
-
+@endsection
 
 
 
